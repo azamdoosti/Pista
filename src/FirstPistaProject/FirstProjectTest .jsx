92a1a03cd0e876ab71270c3a590c6b68ee9cd32e
doosti <a.doosti1@gmail.com>
2023-08-05 15:50:23 +0430

    Adding Expense data to App component

--- src/FirstPistaProject/FirstProjectTest .jsx
+++ src/FirstPistaProject/FirstProjectTest .jsx
@@ -3,9 +3,13 @@ import Expenses from "./Expenses";
 import NewExpense from "./NewExpense/newExpense";
 
 const FirstProjectTest = () => {
+  const addExpenseHandler = (expense) => {
+    console.log("I am in App component");
+    console.log(expense);
+  };
   return (
     <>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses />
     </>
   );
--- src/FirstPistaProject/newExpense/NewExpense.jsx
+++ src/FirstPistaProject/newExpense/NewExpense.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./newExpense.css";
 import ExpenseForm from "../ExpenseForm/ExpenseForm";
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
+    onAddExpense(expenseData);
   };
   return (
     <div className="new-expense">

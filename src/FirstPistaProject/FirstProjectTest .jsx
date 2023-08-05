f692b6439e2e4f2a91d97569e7a4c27e7d2a0a95
doosti <a.doosti1@gmail.com>
2023-08-05 16:07:15 +0430

    passing data to Expenses component

--- src/FirstPistaProject/FirstProjectTest .jsx
+++ src/FirstPistaProject/FirstProjectTest .jsx
@@ -3,6 +3,27 @@ import Expenses from "./Expenses";
 import NewExpense from "./NewExpense/newExpense";
 
 const FirstProjectTest = () => {
+  const expenses = [
+    {
+      id: "a1",
+      title: "toil paper",
+      amount: 93.5,
+      date: new Date(2020, 7, 15),
+    },
+    {
+      id: "a2",
+      title: "toil paper",
+      amount: 90.5,
+      date: new Date(2020, 9, 15),
+    },
+    {
+      id: "a3",
+      title: "toil paper",
+      amount: 91.5,
+      date: new Date(2020, 4, 15),
+    },
+  ];
+
   const addExpenseHandler = (expense) => {
     console.log("I am in App component");
     console.log(expense);
@@ -10,7 +31,7 @@ const FirstProjectTest = () => {
   return (
     <>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses />
+      <Expenses expenses={expenses} />
     </>
   );
 };

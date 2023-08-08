fe115dc756a0fb71820e90b39cd8e6c0130a6e16
doosti <a.doosti1@gmail.com>
2023-08-08 14:18:56 +0430

    create dynamic expense

--- src/FirstPistaProject/FirstProjectTest .jsx
+++ src/FirstPistaProject/FirstProjectTest .jsx
@@ -1,32 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Expenses from "./Expenses";
 import NewExpense from "./NewExpense/newExpense";
+const DUMMY_EXPENSES = [
+  {
+    id: "a1",
+    title: "toil paper",
+    amount: 93.5,
+    date: new Date(2020, 7, 15),
+  },
+  {
+    id: "a2",
+    title: "toil paper",
+    amount: 90.5,
+    date: new Date(2020, 9, 15),
+  },
+  {
+    id: "a3",
+    title: "toil paper",
+    amount: 91.5,
+    date: new Date(2020, 4, 15),
+  },
+];
 
 const FirstProjectTest = () => {
-  const expenses = [
-    {
-      id: "a1",
-      title: "toil paper",
-      amount: 93.5,
-      date: new Date(2020, 7, 15),
-    },
-    {
-      id: "a2",
-      title: "toil paper",
-      amount: 90.5,
-      date: new Date(2020, 9, 15),
-    },
-    {
-      id: "a3",
-      title: "toil paper",
-      amount: 91.5,
-      date: new Date(2020, 4, 15),
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   const addExpenseHandler = (expense) => {
-    console.log("I am in App component");
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    })
+      
+    }
+    /*  console.log("I am in App component");
+    console.log(expense); */
   };
   return (
     <>
